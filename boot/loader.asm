07927f642bb1103c37b1b63d9aae474625cefe5f
zhangxuan2011 <zx20110412@outlook.com>
2025-05-09 13:47:17 +0800

    Added the main code, with multiboot2 header

---
+++ boot/loader.asm
@@ -0,0 +1,37 @@
+; Proka Kernel - A kernel for ProkaOS.
+; Copyright (C) RainSTR Studio 2025, All Rights Reserved.
+;
+; This file contains the header of multiboot2, which can
+; boot up by using GRUB.
+
+; Define a section "multiboot2"
+section .multiboot2
+align 4	   ; The alignment required
+begin:
+    dd 0xE85250D6	; Magic number
+    dd 0 		; Architecture, 0 is protected mode
+    dd end - begin	; Header length
+    dd 0x100000000 - (0xE85250D6 + 0 + (end - begin))	; Checksum
+    
+    ; End tag
+    dw 0	; Tag type, 0 is end
+    dw 0	; Flags
+    dd 8	; Tag size
+end:
+
+; Stack section
+section .bss
+align 16
+stack_bottom:
+    resb 16384 ; 16KB stack, probably enough
+stack_top:
+
+; The entry of the program
+section .text
+global _start
+
+_start:
+    mov esp, stack_top	; Set up the stack pointer
+    ; Todo: Write the main kernel code
+    
+    hlt
