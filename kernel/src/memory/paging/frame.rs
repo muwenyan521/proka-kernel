b9b9fd5db9f6586aa6f9a5eda12530474c4323ec
zhangxuan2011 <zx20110412@outlook.com>
2025-08-27 19:30:18 +0800

    Finished the GLOBAL frame allocator

--- kernel/src/memory/paging/frame.rs
+++ kernel/src/memory/paging/frame.rs
@@ -1,4 +1,9 @@
+extern crate alloc;
+use crate::MEMORY_MAP_REQUEST;
+use alloc::vec::Vec;
 use bitmap_allocator::{BitAlloc, BitAlloc4K};
+use lazy_static::lazy_static;
+use limine::memory_map::EntryType;
 use spin::Mutex;
 use x86_64::PhysAddr;
 use x86_64::structures::paging::{FrameAllocator, FrameDeallocator, PhysFrame, Size4KiB};
@@ -10,11 +15,30 @@ fn addr_to_phys_frame(addr: Option<usize>) -> Option<PhysFrame> {
     PhysFrame::from_start_address(phys_addr).ok()
 }
 
+lazy_static! {
+    pub static ref GLOBAL_FRAME_ALLOCATOR: PhysFrameAlloc = {
+        let mut frame_allocator = PhysFrameAlloc::new();
+        let memory_map = MEMORY_MAP_REQUEST.get_response().unwrap();
+
+        // Collact all usable memory region
+        let mut usable_ranges = Vec::new();
+        for entry in memory_map.entries().iter() {
+            if entry.entry_type == EntryType::USABLE {
+                let start = entry.base as usize;
+                let end = (entry.base + entry.length) as usize;
+                usable_ranges.push((start, end));
+            }
+        }
+
+        // Initialize the frame allocator in ONLY 1 TIME
+        frame_allocator.init(&usable_ranges);
+        frame_allocator
+    };
+}
+
 pub struct PhysFrameAlloc {
-    /// The base address
-    base: usize,
-    /// The end address
-    end: usize,
+    /// The range of the physical frame allocator
+    range: Vec<(usize, usize)>,
     /// The bitmap allocator
     inner: Mutex<BitAlloc4K>,
     /// Whether the allocator is initialized
@@ -24,19 +48,19 @@ pub struct PhysFrameAlloc {
 impl PhysFrameAlloc {
     pub fn new() -> Self {
         Self {
-            base: 0,
-            end: 0,
+            range: Vec::new(),
             inner: Mutex::new(BitAlloc4K::default()),
             initialized: Mutex::new(false),
         }
     }
 
     /// The initializator of physical frame allocator.
-    pub fn init(&mut self, start: usize, end: usize) {
-        self.base = start;
-        self.end = end;
+    pub fn init(&mut self, ranges: &[(usize, usize)]) {
+        self.range = ranges.to_vec();
         let mut guard = self.inner.lock();
-        guard.insert(start..end);
+        for &(start, end) in ranges {
+            guard.insert(start..end); // Add available range to bitmap allocator
+        }
         *self.initialized.lock() = true;
     }
 
@@ -57,15 +81,26 @@ impl PhysFrameAlloc {
             return;
         }
         // Check if the key is within the initialized range
-        if key < self.base || key >= self.end {
-            // This address is not managed by this allocator
+        let mut is_in_managed_range = false;
+        for &(start, end) in &self.range {
+            // The physical frame is 4KiB aligned, so check here
+            if key >= start && key < end && (key % 0x1000 == 0) {
+                is_in_managed_range = true;
+                break;
+            }
+        }
+        if !is_in_managed_range {
+            // the key is NOT in the range of the frame allocator, just ignore
             return;
         }
+
         let mut inner = self.inner.lock();
         if inner.test(key) {
             inner.dealloc(key);
         } else {
             // This frame was not allocated, maybe log an error
+            // Temporary solution: panic
+            panic!("Deallocating unallocated frame");
         }
     }
 }
